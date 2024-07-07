68753fb95fac6d0a9baf1ff0e922e693bf0f2294
Mahak <agrawalmahak0206@gmail.com>
2024-07-07 13:20:21 +0530

    First Commit

---
+++ src/Components/EditEmployee.jsx
@@ -0,0 +1,112 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const EditEmployee = () => {
+    const {id} = useParams()
+    const [employee, setEmployee] = useState({
+        name: "",
+        email: "",
+        salary: "",
+        phone:""
+      });
+      const [category, setCategory] = useState([])
+      const navigate = useNavigate()
+
+      useEffect(()=> {
+        axios.get('')
+        .then(result => {
+            if(result.data.Status) {
+                setCategory(result.data.Result);
+            } else {
+                alert(result.data.Error)
+            }
+        }).catch(err => console.log(err))
+
+        axios.get()
+        .then(result => {
+            setEmployee({
+                ...employee,
+                name: result.data.Result[0].name,
+                email: result.data.Result[0].email,
+                phone: result.data.Result[0].phone
+            })
+        }).catch(err => console.log(err))
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.put()
+        .then(result => {
+            if(result.data.Status) {
+                navigate('/dashboard/employee')
+            } else {
+                alert(result.data.Error)
+            }
+        }).catch(err => console.log(err))
+    }
+    
+  return (
+    <div className="d-flex justify-content-center align-items-center mt-3">
+      <div className="p-3 rounded w-50 border">
+        <h3 className="text-center">Edit Employee</h3>
+        <form className="row g-1" onSubmit={handleSubmit}>
+          <div className="col-12">
+            <label for="inputName" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputName"
+              placeholder="Enter Name"
+              value={employee.name}
+              onChange={(e) =>
+                setEmployee({ ...employee, name: e.target.value })
+              }
+            />
+          </div>
+          <div className="col-12">
+            <label for="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control rounded-0"
+              id="inputEmail4"
+              placeholder="Enter Email"
+              autoComplete="off"
+              value={employee.email}
+              onChange={(e) =>
+                setEmployee({ ...employee, email: e.target.value })
+              }
+            />
+          </div>
+          <div className='col-12'>
+            <label for="inputPhone" className="form-label">
+              Phone
+            </label>
+            <input
+              type="number"
+              className="form-control rounded-0"
+              id="inputPhone"
+              placeholder="Enter Phone"
+              autoComplete="off"
+              value={employee.phone}
+              onChange={(e) =>
+                setEmployee({ ...employee, phone: e.target.value })
+              }
+            />
+          </div>         
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary w-100">
+              Edit Employee
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default EditEmployee
